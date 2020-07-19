f629548361ae23994e55ba88527b673045466ae2
trickste <nishantprakashsaini285@gmail.com>
2020-07-19 16:42:25 +0530

    upload

--- main.tf
+++ main.tf
@@ -6,14 +6,6 @@ terraform {
     }
 }
 	
-terraform {
-    backend "s3"  {
-        bucket = "nishant-terraform-state-bucket-prod"
-		key    = "global/s3/terraform.tfstate"
-        region = "ap-south-1"
-    }
-}
-	
 provider "aws" {
   profile    = "default"
   region     = "ap-south-1"
