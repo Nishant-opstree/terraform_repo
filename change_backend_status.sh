2c3d6bdef5ffea7bf865e9b5973ef47b1785a563
trickste <nishantprakashsaini285@gmail.com>
2020-07-19 16:39:52 +0530

    upload

---
+++ change_backend_status.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+backend_status=`cat backend_status | awk '/s3_backend_configured/{print $2}'`
+if [ "$backend_status" == false ]
+then
+	terraform init
+	terraform plan -target=aws_s3_bucket.prod_terraform_state -target=aws_dynamodb_table.prod_terraform_locks
+	sed -i "/s3_backend_configured/s/false/true/" backend_status
+	backend_data='''terraform {
+    backend "s3"  {
+        bucket = "nishant-terraform-state-bucket-prod"
+		key    = "global/s3/terraform.tfstate"
+        region = "ap-south-1"
+    }
+}
+	'''
+	echo -e "$backend_data" | cat - main.tf > temp && mv temp main.tf
+	echo ".terraform/*" > .gitignore
+	echo "terraform.*" >> .gitignore
+	git add .
+	git commit -m "upload"
+	git push origin prod_master
+
+fi
